ae0db09102ce3495cc4e180695c258e5c14f8ab9
Mohamed Salman <mohamed.akj@gmail.com>
2024-09-22 23:20:30 +0530

    Added new li in navbar.html to render panel app and downgraded panel, bokeh version in requirements.txt and commented bokeh-fastapi package. Updated main to call different panel_app version

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
+from apis.general_pages.panel_app_v144 import create_app, PANEL_PORT
+# Enable below line for panel_app_v150.py
+# from apis.general_pages.panel_app_v150 import create_app
 
 import panel as pn
-from panel.io.fastapi import add_panel_app
+# Enable below line for panel_app_v150.py
+# from panel.io.fastapi import add_application
 
 def include_router(app):
     app.include_router(general_pages_router)
@@ -15,7 +20,13 @@ def start_application():
     
 app = start_application()
 
-@add_panel_app("/panel", app=app, title="My panel app")
-def create_panel_app():
-    slider = pn.widgets.IntSlider(name="Slider", start=0, end=10, value=3)
-    return slider.rx() * '⭐'
+# Enable below lines for panel_app_v150.py
+# @add_application("/panel", app=app, title="My panel app")
+# def create_panel_app():
+#     # slider = pn.widgets.IntSlider(name="Slider", start=0, end=10, value=3)
+#     # return slider.rx() * '⭐'
+#     return create_app()
+
+@app.get("/panel")
+async def panel_redirect(request: Request):
+    return RedirectResponse(url=f"http://{request.url.hostname}:{PANEL_PORT}/")
