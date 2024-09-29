0f09a69d9a269352755328f103e8c98dab4e2390
Mohamed Salman <mohamed.akj@gmail.com>
2024-09-29 07:53:06 +0530

    removed v1.4.4 related codes and updated main.py and requirements.txt to use latest Panel v1.5.0

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
-from apis.general_pages.panel_app_v144 import create_app, PANEL_PORT
-# Enable below line for panel_app_v150.py
-# from apis.general_pages.panel_app_v150 import create_app
+from apis.general_pages.panel_app import create_app
 
 import panel as pn
-# Enable below line for panel_app_v150.py
-# from panel.io.fastapi import add_application
+from panel.io.fastapi import add_application
 
 def include_router(app):
     app.include_router(general_pages_router)
@@ -20,13 +17,8 @@ def start_application():
     
 app = start_application()
 
-# Enable below lines for panel_app_v150.py
-# @add_application("/panel", app=app, title="My panel app")
-# def create_panel_app():
-#     # slider = pn.widgets.IntSlider(name="Slider", start=0, end=10, value=3)
-#     # return slider.rx() * '⭐'
-#     return create_app()
-
-@app.get("/panel")
-async def panel_redirect(request: Request):
-    return RedirectResponse(url=f"http://{request.url.hostname}:{PANEL_PORT}/")
+@add_application("/panel", app=app, title="My panel app")
+def create_panel_app():
+    # slider = pn.widgets.IntSlider(name="Slider", start=0, end=10, value=3)
+    # return slider.rx() * '⭐'
+    return create_app()
